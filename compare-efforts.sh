d3b6a4e1d58eaaebf068fd557bf3140e66809e7a
Hannu Hartikainen <hannu.hartikainen@gmail.com>
2018-11-11 22:19:59 +0200

    Print efforts for "normal" layouts, esp. tarmak
    Needs a simple modification to carpalx:
    
    elsif ($action =~ "printeffort") {
        printf("%.3f", calculate_effort($keytriads, $keyboard));
    }

---
+++ compare-efforts.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+for f in baseline-layouts/*.conf optimized*/*.conf; do
+    perl ./carpalx-0.12/bin/carpalx \
+        -conf ./single-swap.conf \
+        -keyboard_input "$f" \
+        -action loadkeyboard,loadtriads,printeffort,quit
+    echo "   $(basename $f .conf)"
+done
